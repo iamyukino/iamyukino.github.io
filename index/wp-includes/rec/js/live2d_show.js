6f44bfebdc665116ed61d45a10ff12211145b3dd
雨宮ゆきの <iamyukino@outlook.com>
2022-04-26 23:14:01 +0800

    add 3 live2d models

---
+++ index/wp-includes/rec/js/live2d_show.js
@@ -0,0 +1,16 @@
+function back2lastdoc (href){
+    if (history.length > 1) window.history.go(-1);
+    else window.location.href = "https://iamyukino.github.io/rec/" + href + "/";
+}
+function loadmd (name){
+    let modulesPath = "../../index/wp-content/rec/live2d/models/"
+	let modelNames = [name]
+	loadModel(modulesPath, modelNames);
+}
+function copy_code(){
+    live2d-source.select();
+    document.execCommand("copy")
+}
+function loading_live2d_before() {
+    document.getElementById("live2d").style.backgroundImage="none";
+}
